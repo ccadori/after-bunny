f0ddf2804a07486733150d591b5a70125a8c9954
Carlos Cadori <carloscadori.dev@gmail.com>
2016-11-25 21:30:24 -0200

    trap graphics, animation, and behavior

---
+++ Assets/Scripts/Trap/Trap.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+public class Trap : MonoBehaviour 
+{
+	// Inspector
+	[SerializeField]
+	private float raycastRange;
+	[SerializeField]
+	private LayerMask affectedlayers;
+	// Inspector
+
+	private bool closed;
+	private Animator anim;
+
+	void Start()
+	{
+		closed = false;
+		anim = GetComponent<Animator> ();
+	}
+
+	public void OnClose()
+	{
+		//RaycastHit2D hit = Physics2D.Raycast (transform.position, Vector3.forward, raycastRange, affectedlayers);
+	}
+
+	public void OnOpen()
+	{
+		closed = false;
+	}
+
+	void Close()
+	{
+		if (closed)
+			return;
+
+		closed = true;
+		anim.SetBool ("Closed", closed);
+	}
+
+	void Open()
+	{
+		if (!closed)
+			return;
+		
+		anim.SetBool ("Closed", false);
+	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.collider.CompareTag ("Player"))
+			Close ();
+	}
+}
