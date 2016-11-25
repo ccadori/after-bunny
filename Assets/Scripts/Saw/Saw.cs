7b2fe27d6411f1877f2114f334c6190d48b6033e
Carlos Cadori <carloscadori.dev@gmail.com>
2016-11-25 20:10:48 -0200

    Saw graphics, physics, and spin

---
+++ Assets/Scripts/Saw/Saw.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Saw : MonoBehaviour 
+{
+	// Inspector
+	[SerializeField]
+	[Range(0, 100)]
+	private float spinVelocity;
+	// Inspector
+
+	private Rigidbody2D body;
+
+	void Start()
+	{
+		body = GetComponent<Rigidbody2D> ();
+		Spin ();
+	}
+
+	void Spin()
+	{
+		body.AddTorque (spinVelocity * Time.deltaTime * 100);
+	}
+}
